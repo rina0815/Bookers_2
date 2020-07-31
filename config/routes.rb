9de36259a41457581ea8db3aeeaca5973650fe73
vagrant <vagrant@localhost.localdomain>
2020-07-31 15:09:39 +0000

    [Add}Home,About,Usersリンク連携、ログイン後リダイレクト先の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'homes/home'
+  root 'homes#home'
+  get 'home/about' => 'homes#about'
+  	
   devise_for :users
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
