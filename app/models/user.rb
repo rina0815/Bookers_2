f7f5367a9d7b14b0bcdc94c9b17886d74a4017c3
vagrant <vagrant@localhost.localdomain>
2020-08-01 11:44:31 +0000

    [Add]ログイン認証変更、フラッシュメッセージ追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name,
+  length: { minimum: 2, maximum: 20, message: "is too short (minimum is 2 characters)" }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'homes#home'
   get 'home/about' => 'homes#about'
-  	
   devise_for :users
   resources :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
