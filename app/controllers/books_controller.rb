20689dd2bb1b6be0e28e5f8605189826ade6fc24
vagrant <vagrant@localhost.localdomain>
2020-08-03 11:45:02 +0000

    [Add]usersのindex,showページ作成まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,11 +8,16 @@ class BooksController < ApplicationController
   	@book = Book.new(book_params)
   	@book.user_id = current_user.id
     if @book.save
-  	   redirect_to book_path(@book), notice: 'You have updated book successfully.'
+  	   redirect_to book_path(@book), notice: 'You have created book successfully.'
     else
       @books = Book.all
       render 'index'
     end
+  end
+
+  def show
+    @new_book = Book.new
+    @book = Book.find(params[:id])
   end
 
   def edit
@@ -21,16 +26,14 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
-    redirect_to book_path(@book)
+    redirect_to book_path(@book), notice: 'You have updated book successfully.'
   end
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  def show
-    @book = Book.find(params[:id])
-  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
