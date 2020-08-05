dc96d421eeaebe30c03dd2eabcf27a2e12fea96a
vagrant <vagrant@localhost.localdomain>
2020-08-05 07:58:06 +0000

    [Update]大文字・小文字修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -51,7 +51,7 @@ class BooksController < ApplicationController
   def correct_user
     @book = Book.find(params[:id])
     if @book.user_id != current_user.id
-      redirect_to user_path(current_user)
+      redirect_to books_path
     end
   end
 end
